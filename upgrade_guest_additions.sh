929a760d8b35a1c49053a79bc136c4b264de914a
alan7s <alan7@outlook.com>
2023-04-11 22:37:54 -0300

    Create upgrade_guest_additions.sh

---
+++ upgrade_guest_additions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd /tmp
+if wget 'http://download.virtualbox.org/virtualbox/7.0.6/VBoxGuestAdditions_7.0.6.iso'; then
+    if sudo mount VBoxGuestAdditions_7.0.6.iso -o loop /mnt; then
+        if sudo sh /mnt/VBoxLinuxAdditions.run; then
+            sudo umount /mnt
+            sudo reboot
+        else
+            echo "Erro ao executar o script de instalação das adições de convidado do VirtualBox."
+        fi
+    else
+        echo "Erro ao montar o arquivo ISO das adições de convidado do VirtualBox."
+    fi
+else
+    echo "Erro ao fazer o download do arquivo ISO das adições de convidado do VirtualBox."
+fi
